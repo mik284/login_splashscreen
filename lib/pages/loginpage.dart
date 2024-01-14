5d26ec2f044980b5cde40021d5913f1458847bea
mik284 <mikeykamau222@gmail.com>
2024-01-14 11:30:55 +0300

    add appbar :modified

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget{
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     body: Padding(
-       padding: const EdgeInsets.all(8.0),
-       child: AppBar(
-         leading: CloseButtonIcon(),
-         title: Text('Login'),
-         actions: [
-           Text("skip"),
-         ],
-       ),
-     ),
-   );
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: AppBar(
+          leading: Column(
+            children: [CloseButtonIcon(), Text("CLOSE")],
+          ),
+          title: Center(child: Text('Sign in')),
+          actions: [
+            Text("SKIP FOR NOW"),
+          ],
+        ),
+      ),
+    );
   }
-
 }
