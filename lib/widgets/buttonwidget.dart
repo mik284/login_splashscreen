3c6a7e1c22abf3cb951ce327766dccfc93efc146
mik284 <mikeykamau222@gmail.com>
2024-01-14 20:54:59 +0300

    add button widget

---
+++ lib/widgets/buttonwidget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  final Color color;
+  final double width;
+  final double height, radius;
+  final Widget widget;
+
+  const ButtonWidget(
+      {super.key,
+        required this.color,
+        required this.width,
+        required this.height,
+        required this.radius,
+        required this.widget});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+          color: color, borderRadius: BorderRadius.circular(radius)),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Center(child: widget),
+      ),
+    );
+  }
+}
