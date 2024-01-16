f6ae0e1221939e27532dfaef024f8d1895ba5a9f
mik284 <mikeykamau222@gmail.com>
2024-01-16 10:36:48 +0300

    updated body add bottomnav

--- lib/pages/loginui_revamp.dart
+++ lib/pages/loginui_revamp.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:loginui/widgets/buttonwidget.dart';
 
 import '../widgets/textwidget.dart';
 
@@ -8,11 +9,36 @@ class LoginUi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List mylist = [
+      {
+        "img": const SizedBox(),
+        "title": "👋 Welcome to Beamo",
+        "color": Colors.transparent
+      },
+      {
+        "img": const Icon(Icons.mail_outline, size: 30),
+        "title": "Login with Email",
+        "color": Colors.white
+      },
+      {
+        "img": const Icon(Icons.account_circle, size: 30),
+        "title": "Login with Google",
+        "color": Colors.white
+      },
+      {
+        "img": const Icon(
+          Icons.facebook,
+          color: Colors.white, size: 30,
+        ),
+        "title": "Login with Facebook",
+        "color": Colors.blueAccent
+      },
+    ];
     return Scaffold(
       appBar: AppBar(
         leading: const CloseButtonIcon(),
         bottom: const PreferredSize(
-            preferredSize: Size(30, 10),
+            preferredSize: Size(30, 5),
             child: Align(
               alignment: Alignment.centerLeft,
               child: Padding(
@@ -23,7 +49,7 @@ class LoginUi extends StatelessWidget {
                     align: TextAlign.center,
                     family: "Montserrat",
                     weight: FontWeight.w300,
-                    size: 10),
+                    size: 12),
               ),
             )),
         title: const TextWidget(
@@ -35,16 +61,68 @@ class LoginUi extends StatelessWidget {
             size: 18),
         centerTitle: true,
         actions: const [
-          TextWidget(
-              text: 'SKIP FOR NOW',
-              color: Colors.black,
-              align: TextAlign.center,
-              family: "Montserrat",
-              weight: FontWeight.w500,
-              size: 10),
+          Padding(
+            padding: EdgeInsets.only(right: 15.0),
+            child: TextWidget(
+                text: 'SKIP FOR NOW',
+                color: Colors.black,
+                align: TextAlign.center,
+                family: "Montserrat",
+                weight: FontWeight.w500,
+                size: 10),
+          ),
         ],
       ),
-      body: ,
+      body: ListView.builder(
+          itemCount: mylist.length,
+          itemBuilder: (context, index) {
+            final item = mylist[index];
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: index == 0 ? 38.0 : 0),
+                  child: ButtonWidget(
+                      color: item['color'],
+                      currentview: index == 0 || index == 3 ? "welcome-text" : "",
+                      width: 360,
+                      height: 70,
+                      radius: 50,
+                      widget: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          item["img"] as Widget,
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          TextWidget(
+                              text: item['title'],
+                              color: index == 3 ? Colors.white : Colors.black,
+                              family: "Montserrat",
+                              size: index == 0 ? 18 : 13,
+                              weight: FontWeight.w400,
+                              align: TextAlign.center),
+                        ],
+                      )),
+                )
+              ],
+            );
+          }),
+      bottomNavigationBar: const ButtonWidget(
+          color: Color(0xff80bac2),
+          width: 360,
+          height: 50,
+          radius: 40,
+          currentview: "bottom",
+          widget: TextWidget(
+              text: "Next",
+              color: Colors.white,
+              family: "Montserrat",
+              size: 13,
+              weight: FontWeight.w400,
+              align: TextAlign.center)),
     );
   }
 }
